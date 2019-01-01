c243845c60332aaf18e043ca5a0d70999f84788e
atenagm <ashenatena@gmail.com>
2019-01-01 17:04:02 +0330

    add function longest_common_susequence

--- LCSQ/main.py
+++ LCSQ/main.py
@@ -1,4 +1,32 @@
 import sys
+from numpy import zeros
+
+
+def longest_common_subsequence(dna_list):
+    m = len(dna_list[0])
+    n = len(dna_list[1])
+    grid = zeros((m + 1, n + 1))
+
+    # filling the grid
+    for i in range(m):
+        for j in range(n):
+            if dna_list[0][i] == dna_list[1][j]:
+                grid[i + 1][j + 1] = grid[i][j] + 1
+            else:
+                grid[i + 1][j + 1] = max(grid[i][j + 1], grid[i + 1][j])
+
+    # find LCS using the grid
+    lcs = ""
+    while m * n > 0:
+        if grid[m][n] == grid[m][n - 1]:
+            n -= 1
+        elif grid[m][n] == grid[m - 1][n]:
+            m -= 1
+        else:
+            lcs += dna_list[0][m - 1]
+            m -= 1
+            n -= 1
+    return lcs[::-1]
 
 
 def load_data(path):
@@ -11,9 +39,10 @@ def load_data(path):
                 index += 1
                 dna_list.append("")
                 continue
-            dna_list[index] += line
+            dna_list[index] += line.replace("\n", "")
 
     return dna_list
 
 
 dna_sequences = load_data(sys.argv[1])
+print(longest_common_subsequence(dna_sequences))
